1e676d52db7f3559d01134bd6ab9679b618dfa24
azzynovais <arielcloss@gmail.com>
2025-04-03 11:17:19 -0400

    Hello World e Atividade 1!

---
+++ Atividade_01.js
@@ -0,0 +1,9 @@
+let nome = "Azzy Novais";
+let idade = 16;
+console.log("Olá meu nome é", nome, "E tenho", idade, "Anos");
+let palavra = "Olá mundo";
+console.log(palavra.toUpperCase())
+console.log(palavra.toLowerCase())
+let altura = 1500;
+let peso = 100;
+console.log(altura / peso);
