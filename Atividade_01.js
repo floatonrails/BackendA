04ec5824803c57c4f165cca48787b132c30e5b74
azzynovais <arielcloss@gmail.com>
2025-04-10 10:08:34 -0400

    Segundo Commit!

--- Atividade_01.js
+++ Atividade_01.js
@@ -1,9 +1,14 @@
+// Atividade 01 e Atividade 02
 let nome = "Azzy Novais";
 let idade = 16;
-console.log("Olá meu nome é", nome, "E tenho", idade, "Anos");
+console.log("Olá meu nome é " +nome+ " E tenho " +idade+ " Anos");
 let palavra = "Olá mundo";
+//Atividade 03
 console.log(palavra.toUpperCase())
 console.log(palavra.toLowerCase())
-let altura = 1500;
-let peso = 100;
-console.log(altura / peso);
+console.log(typeof idade)
+// Atividade 04
+let altura = 1;81;
+let peso = 130; 
+let imc = (peso / (altura * altura)).toFixed(2);
+console.log("Seu IMC é: " +imc)
