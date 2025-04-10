4d94c1684377fe028edcae9274e0c9fcf8516667
azzynovais <arielcloss@gmail.com>
2025-04-10 11:31:17 -0400

    Quarto Commit!

---
+++ Atividade_02.js
@@ -0,0 +1,22 @@
+let numero1 = 16;
+let numero2 = 18;
+let calculoadicao = numero1 + numero2;
+let calculosubtracao = numero1 - numero2;
+let calculomultiplcacao = numero1 * numero2;
+let calculodivisao = numero1 / numero2;
+let calculomodudlo = numero1 % numero2;
+
+console.log (calculoadicao, calculosubtracao, calculomultiplcacao, calculomodudlo); 
+let idade1 = 14;
+let idade2 = 15;
+let comparacao = idade1 != idade2;
+
+console.log('A idade da idade1 comparada a idade2 é:' +comparacao)  
+
+let idade3 = 18;
+let comparacao2 = idade3 >= 18 ?  "Maior de idade" : "Menor de idade";
+console.log(comparacao2)
+
+let numero3 = 3
+let comparacao3 = numero3 %1 ? "Par" : "Impar";
+console.log(comparacao3)
